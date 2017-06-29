ad71c4b5c6a72894e2dedb30f9a2d1525ec9b199
rgriff23 <rgriff23@gmail.com>
2017-06-29 13:11:29 -0400

    change profile

---
+++ _drafts/Untitled.R
@@ -0,0 +1,29 @@
+################
+# PREPARATIONS #
+################
+
+# Load packages
+
+# Twitter authentication (customize this for yourself to reproduce analysis)
+
+#################
+# CREATE CORPUS #
+#################
+
+# Download tweets
+
+# Generate corpus
+
+# Clean corpus
+
+# Export cleaned data
+
+#########################
+# TIMELINE OF THE STORM #
+#########################
+
+
+
+#######
+# END #
+#######
