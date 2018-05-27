870be2993e648185bfc49a829edf16621912f6d6
Randi Griffin <rgriff23@users.noreply.github.com>
2018-05-27 12:46:04 -0400

    fix bug
    change extension from Rmd to md in post

--- _drafts/knit_post.R
+++ _drafts/knit_post.R
@@ -11,15 +11,18 @@ knit_post <- function(file) {
   # SAVE CURRENT DIRECTORY
   current_dir <- getwd()
   
+  # CHANGE EXTENSION
+  file2 <- gsub(".Rmd", ".md", file)
+  
   # KNIT AND SEND TO POSTS FOLDER
   if (windows == TRUE) {
     setwd("C://Users//Randi Griffin//Documents//GitHub//rgriff23.github.io//_drafts/")
     knitr::knit(input = file,
-                output = paste0("C://Users//Randi Griffin//Documents//GitHub//rgriff23.github.io//_posts//", file))
+                output = paste0("C://Users//Randi Griffin//Documents//GitHub//rgriff23.github.io//_posts//", file2))
   } else {
     setwd("~/Documents/GitHub/rgriff23.github.io/_drafts/")
     knitr::knit(input = file,
-                output = paste0("~/Documents/GitHub/rgriff23.github.io/_posts/", file))
+                output = paste0("~/Documents/GitHub/rgriff23.github.io/_posts/", file2))
   }
   
   # RETURN TO ORIGINAL DIRECTORY
